cb6f29acaf32feffcbbe9429d4aba2e5f03e87ba
al <paet@mail.ru>
2023-03-03 23:58:49 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Renault Duster", 2014, 640000, "Желтый", 102);
+        car1.getInfo();
+        car1.start();
+        car1.road("Миасс");
+        car1.stop();
+        car1.road("Миасс");
+
+        Car car2 = new Car("Toyota Land Cruiser 200", 2008, 2200000, "Черный", 235);
+        car2.getInfo();
+        car2.start();
+        car2.road("Киров");
+        car2.stop();
+        car2.road("Киров");
+
+        car1.yearEquals(car2);
+        car2.powerEquals(car1);
+
+        System.out.println();
+
+        System.out.println(car1.getModel() + " стоит " + car1.getPrice() + " рублей");
+        car1.setPrice(1000000);
+        System.out.println(car1.getModel() + " стоит " + car1.getPrice() + " рублей");
+    }
+}
